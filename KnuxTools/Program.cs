25471c34c95479ee6ce84c09024975c467342163
Knux <knuxfan@live.co.uk>
2023-02-21 22:25:00 +0000

    KnuxTools SCO Import Support
    Also contains commented out structuring for multi selecting. Shamelessly ripped from Marathon.CLI

--- KnuxTools/Program.cs
+++ KnuxTools/Program.cs
@@ -22,8 +22,35 @@ namespace KnuxTools
                         Console.WriteLine($"File: {arg}\n");
 
                         // Treat this file differently depending on type.
-                        switch (Path.GetExtension(arg))
+                        switch (Path.GetExtension(arg).ToLower())
                         {
+                            // Seralised Models
+                            case ".fbx":
+                            case ".dae":
+                            case ".obj":
+                                //Console.WriteLine
+                                //(
+                                //    "This file is a generic seralised type, please specify what format it is;\n" +
+                                //    "1. CarZ Engine Model"
+                                //);
+
+                                //switch (Console.ReadKey().KeyChar)
+                                //{
+                                //    case '1':
+                                        using (KnuxLib.Engines.CarZ.SCO sco = new())
+                                        {
+                                            sco.ImportAssimp(arg);
+                                            sco.Save($@"{Path.GetDirectoryName(arg)}\{Path.GetFileNameWithoutExtension(arg)}.sco");
+                                        }
+                                        using (KnuxLib.Engines.CarZ.MaterialLibrary mat = new())
+                                        {
+                                            mat.ImportAssimp(arg);
+                                            mat.Save($@"{Path.GetDirectoryName(arg)}\{Path.GetFileNameWithoutExtension(arg)}.mat");
+                                        }
+                                //        break;
+                                //}
+                                break;
+
                             // CarZ Engine Formats
                             case ".mat": using (KnuxLib.Engines.CarZ.MaterialLibrary mat = new(arg, true)) break;
                             case ".sco": using (KnuxLib.Engines.CarZ.SCO sco = new(arg, true)) break;
