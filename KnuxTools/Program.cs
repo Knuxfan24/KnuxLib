17c69f6b9184d4111f887a0285e8d1d1299362d2
Knux <knuxfan@live.co.uk>
2023-02-22 02:09:13 +0000

    (KnuxTools) WAD stuff
    Also contains some skeleton stuff for when I readd stuff like the Sonic 4 archives.

--- KnuxTools/Program.cs
+++ KnuxTools/Program.cs
@@ -15,6 +15,18 @@ namespace KnuxTools
                 // Loop through each argument.
                 foreach (string arg in args)
                 {
+                    // Directory based checks.
+                    if (Directory.Exists(arg))
+                    {
+                        // Ask the user what to pack this folder as (currently disabled as the only archive format I support is the Gods Engine WADs and I haven't written import stuff yet).
+                        //Console.WriteLine
+                        //(
+                        //    "Please specify the archive type to pack this directory into;\n" +
+                        //    "1. Gods Engine WAD File"
+                        //);
+                        break;
+                    }
+
                     // File based checks.
                     if (File.Exists(arg))
                     {
@@ -28,12 +40,14 @@ namespace KnuxTools
                             case ".fbx":
                             case ".dae":
                             case ".obj":
+                                // Ask the user what to convert this model to (currently disabled as only the CarZ SCO format has this.
                                 //Console.WriteLine
                                 //(
                                 //    "This file is a generic seralised type, please specify what format it is;\n" +
                                 //    "1. CarZ Engine Model"
                                 //);
 
+                                // Convert the model to the selected format.
                                 //switch (Console.ReadKey().KeyChar)
                                 //{
                                 //    case '1':
@@ -54,6 +68,24 @@ namespace KnuxTools
                             // CarZ Engine Formats
                             case ".mat": using (KnuxLib.Engines.CarZ.MaterialLibrary mat = new(arg, true)) break;
                             case ".sco": using (KnuxLib.Engines.CarZ.SCO sco = new(arg, true)) break;
+
+                            // Gods Engine Formats
+                            case ".wad":
+                                // Ask the user for the WAD version.
+                                Console.WriteLine
+                                        (
+                                            "This file has multiple variants that can't be auto detected, please specifiy the variant;\n" +
+                                            "1. Ninjabread Man PC/PS2\n" +
+                                            "2. Ninjabread Man Wii"
+                                        );
+
+                                // Extract the WAD according to the selected version.
+                                switch (Console.ReadKey().KeyChar)
+                                {
+                                    case '1': using (KnuxLib.Engines.Gods.WAD wad = new(arg, KnuxLib.Engines.Gods.WAD.FormatVersion.NinjabreadMan_PCPS2, true)) break;
+                                    case '2': using (KnuxLib.Engines.Gods.WAD wad = new(arg, KnuxLib.Engines.Gods.WAD.FormatVersion.NinjabreadMan_Wii, true)) break;
+                                }
+                                break;
 
                             // ProjectM Engine Formats
                             case ".dat": using (KnuxLib.Engines.ProjectM.MessageTable messageTable = new(arg, true)) break;
