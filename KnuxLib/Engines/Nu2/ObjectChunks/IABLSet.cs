f03e24508a56364fc0688352f5ecb25dc6ad34d8
Knux <knuxfan@live.co.uk>
2023-04-21 03:51:25 +0100

    (Nu2) Add placeholder IABL

---
+++ KnuxLib/Engines/Nu2/ObjectChunks/IABLSet.cs
@@ -0,0 +1,18 @@
+﻿using static KnuxLib.Engines.Nu2.Scene;
+
+namespace KnuxLib.Engines.Nu2.ObjectChunks
+{
+    // TODO: What does IABL stand for?
+    // TODO: What does the data in this chunk actually do?
+    // TODO: Read ANY of this data.
+    public class IABLSet
+    {
+        /// <summary>
+        /// Reads this NuObject chunk and returns a list of the data within.
+        /// </summary>
+        /// <param name="reader">The Marathon BinaryReader to use.</param>
+        public static void Read(BinaryReaderEx reader, FormatVersion version)
+        {
+        }
+    }
+}
