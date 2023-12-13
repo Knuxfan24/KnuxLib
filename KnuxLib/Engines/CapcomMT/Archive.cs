a038ba3ae5dae1b5ba2e8a04be52804e4e755aeb
Knux <knuxfan@live.co.uk>
2023-12-13 00:58:53 +0000

    (CapcomMT) MMXLC extensions
    Added extensions for every type that shows up in both Megaman X Legacy Collections, almost all of these extensions are assumed based on signatures in the file headers that seem to always be consistent.

--- KnuxLib/Engines/CapcomMT/Archive.cs
+++ KnuxLib/Engines/CapcomMT/Archive.cs
@@ -15,27 +15,70 @@
         // Classes for this format.
         readonly Dictionary<uint, string> FileExtensions = new()
         {
-            { 0x06C3DBAA, ".bin" },
-            { 0x6AE91701, ".sld" },
-            { 0x328F438B, ".osd" },
-            { 0x15E8853F, ".emi" },
-            { 0x23D4C76D, ".tam" },
-            { 0x3AE9C67F, ".red" },
-            { 0x4FA96078, ".cl2" },
-            { 0x5B9EA119, ".dem" },
-            { 0x724DF879, ".wav" },
-            { 0x241F5DEB, ".tex" }, // Not In OG
-            { 0x1BCC4966, ".srq" }, // Not In OG
-            { 0x15D782FB, ".sbk" }, // Not In OG
-            { 0x167DBBFF, ".stg" }, // Not In OG
-            { 0x185365EA, ".emp" }, // Not In OG
-            { 0x7808EA10, ".rtx" }, // Not In OG
-            { 0x7772787E, ".evt" },
-            { 0x0589CBA3, ".mcb" },
-            { 0x311E683F, ".rl" },
-            { 0x31BF570E, ".set" },
-            { 0x1E3EE6FB, ".wsx" },
-            { 0x1C7858A2, ".lpk" }
+            { 0x02358E1A, ".spk" },  // Unconfirmed, assumed from file header signature.
+            { 0x0589CBA3, ".mcb" },  // Sourced from old PC version of Megaman X8.
+            { 0x05C5AC17, ".pat" },  // Unconfirmed, assumed from file header signature.
+            { 0x064A3AD8, ".xml" },  // Unconfirmed, assumed from formatting.
+            { 0x06C3DBAA, ".bin" },  // Sourced from old PC version of Megaman X7.
+            { 0x06DEC69C, ".fuv" },  // Unconfirmed, assumed from file header signature.
+            { 0x07F768AF, ".gii" },  // Unconfirmed, assumed from file header signature.
+            { 0x11320E86, ".omp" },  // Unconfirmed, assumed from file header signature.
+            { 0x15D782FB, ".sbk" },  // Unconfirmed, assumed from file header signature.
+            { 0x15E8853F, ".emi" },  // Sourced from old PC version of Megaman X7.
+            { 0x167DBBFF, ".stgr" }, // Unconfirmed, assumed from file header signature.
+            { 0x185365EA, ".emp" },  // Unconfirmed, assumed from file header signature.
+            { 0x1BCC4966, ".srq" },  // Unconfirmed, assumed from file header signature.
+            { 0x1C7858A2, ".lpk" },  // Sourced from old PC version of Megaman X8.
+            { 0x1E3EE6FB, ".wsx" },  // Sourced from old PC version of Megaman X8.
+            { 0x1EC24743, ".ctex" }, // Unconfirmed, assumed from file header signature.
+            { 0x1EF7D5BD, ".fst" },  // Unconfirmed, assumed from file header signature.
+            { 0x219D76EB, ".gdsd" }, // Unconfirmed, assumed from file header signature.
+            { 0x22948394, ".gui" },  // Unconfirmed, assumed from file header signature.
+            { 0x232E228C, ".revr" }, // Unconfirmed, assumed from file header signature.
+            { 0x23D4C76D, ".tam" },  // Sourced from old PC version of Megaman X7.
+            { 0x241F5DEB, ".tex" },  // Unconfirmed, assumed from file header signature.
+            { 0x242BB29A, ".gmd" },  // Unconfirmed, assumed from file header signature.
+            { 0x255D51CD, ".sngw" }, // Unconfirmed, assumed from file header signature.
+            { 0x2609378F, ".cof" },  // Unconfirmed, assumed from file header signature.
+            { 0x2749C8A8, ".mrl" },  // Unconfirmed, assumed from file header signature.
+            { 0x2D462600, ".gfd" },  // Unconfirmed, assumed from file header signature.
+            { 0x311E683F, ".rl" },   // Sourced from old PC version of Megaman X8.
+            { 0x31BF570E, ".set" },  // Sourced from old PC version of Megaman X8.
+            { 0x328F438B, ".osd" },  // Sourced from old PC version of Megaman X7.
+            { 0x39C52040, ".lcm" },  // Unconfirmed, assumed from file header signature.
+            { 0x3AE9C67F, ".red" },  // Sourced from old PC version of Megaman X7.
+            { 0x3C89A0B1, ".tlkd" }, // Unconfirmed, assumed from file header signature.
+            { 0x3F481835, ".rlst" }, // Unconfirmed, assumed from file header signature.
+            { 0x3FD1FBD2, ".mnl" },  // Unconfirmed, assumed from file header signature.
+            { 0x40D2ABF8, ".weak" }, // Unconfirmed, assumed from file header signature.
+            { 0x4323D83A, ".xfs" },  // Unconfirmed, assumed from file header signature.
+            { 0x451EAA25, ".oprm" }, // Unconfirmed, assumed from file header signature.
+            { 0x4A0DE37E, ".dat4" }, // Unconfirmed, assumed from file header signature.
+            { 0x4B575FD0, ".datk" }, // Unconfirmed, assumed from file header signature.
+            { 0x4C0B6650, ".comb" }, // Unconfirmed, assumed from file header signature.
+            { 0x4C0DB839, ".sdl" },  // Unconfirmed, assumed from file header signature.
+            { 0x4CCD72C3, ".gdat" }, // Unconfirmed, assumed from file header signature.
+            { 0x4E397417, ".ean" },  // Unconfirmed, assumed from file header signature.
+            { 0x4FA96078, ".cl2" },  // Sourced from old PC version of Megaman X7.
+            { 0x5344F0C7, ".mpl" },  // Unconfirmed, assumed from file header signature.
+            { 0x541C83B1, ".dmg" },  // Unconfirmed, assumed from file header signature.
+            { 0x55B2ABCA, ".tlkl" }, // Unconfirmed, assumed from file header signature.
+            { 0x589E9D20, ".stff" }, // Unconfirmed, assumed from file header signature.
+            { 0x58A15856, ".mod" },  // Unconfirmed, assumed from file header signature.
+            { 0x59633D17, ".xbgm" }, // Unconfirmed, assumed from file header signature.
+            { 0x5B9EA119, ".dem" },  // Sourced from old PC version of Megaman X7.
+            { 0x619CF7E7, ".col" },  // Unconfirmed, assumed from file header signature.
+            { 0x6AE91701, ".sld" },  // Sourced from old PC version of Megaman X7.
+            { 0x6B685B4E, ".dfc" },  // Unconfirmed, assumed from file header signature.
+            { 0x6BC1BAC7, ".ocl" },  // Unconfirmed, assumed from file header signature.
+            { 0x6D5AE854, ".efl" },  // Unconfirmed, assumed from file header signature.
+            { 0x710B8A11, ".scrl" }, // Unconfirmed, assumed from file header signature.
+            { 0x724DF879, ".wav" },  // Unconfirmed, assumed from formatting.
+            { 0x74D08E45, ".gdsi" }, // Unconfirmed, assumed from file header signature.
+            { 0x7772787E, ".evt" },  // Sourced from old PC version of Megaman X8.
+            { 0x7808EA10, ".rtx" },  // Unconfirmed, assumed from file header signature.
+            { 0x7C322EDC, ".medl" }, // Unconfirmed, assumed from file header signature.
+            { 0x7E8B2835, ".wpl" }   // Unconfirmed, assumed from file header signature.
         };
 
         // Actual data presented to the end user.
@@ -82,7 +125,7 @@
                 int compressedSize = reader.ReadInt32();
 
                 // Read an unknown floating point value.
-                // TODO: What is this? Some sort of compression ratio read out maybe? Is important in some way.
+                // TODO: What is this? Some sort of compression ratio read out maybe? Is important in some way. Skyth thinks this could be a CRC32 hash of some sort.
                 float unknownFloat = reader.ReadSingle();
 
                 // Read the offset to this file's data.
